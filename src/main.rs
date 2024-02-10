918bdf0163b731af32025481dde78297f7288010
m1a2st <s7133700@gmail.com>
2024-02-10 19:38:05 +0800

    init(server): add server stuct
    create server struct and run function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Server {
+            addr,
+        }
+    }
+
+    fn run(self) {
+
+        println!("Server is running on {}", self.addr);
+    }
+}
+
 fn main() {
-    let server  = Server::new("127.0.0.1:8080");
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
