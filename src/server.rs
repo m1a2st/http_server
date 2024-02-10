c162b7c0fcbbc74d8efbf36d8166dac10ecf532b
m1a2st <s7133700@gmail.com>
2024-02-10 22:33:27 +0800

    feat(server): add server tcp listnener
    add tcp lidtener and match syntax

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,29 @@
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
 
 impl Server {
     pub fn new(addr: String) -> Self {
-        Server {
+        Self {
             addr,
         }
     }
 
     pub fn run(self) {
         println!("Server is running on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((stream, addr)) => {
+                    println!("A new connection!");
+
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
     }
 }
