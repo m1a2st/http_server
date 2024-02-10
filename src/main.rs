13052f4b80db669e69cd358c4f1af51071ec5bf9
m1a2st <s7133700@gmail.com>
2024-02-10 20:51:18 +0800

    init(request): add request struct
    write http request first line struct and add http mehtod enum

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,34 @@ impl Server {
     }
 
     fn run(self) {
-
         println!("Server is running on {}", self.addr);
     }
 }
 
 fn main() {
+    let get = Method::GET;
+    let delete = Method::DELETE;
+
+
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
 }
