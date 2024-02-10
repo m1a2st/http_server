f038ac920a3e2e17a1e090beed1bb94773e779d2
m1a2st <s7133700@gmail.com>
2024-02-10 20:58:39 +0800

    fix(query_string): change to Option
    change query_string type to Option to prevent none value

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,13 @@ impl Server {
 }
 
 fn main() {
-    let get = Method::GET;
-    let delete = Method::DELETE;
-
-
-
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
 struct Request {
     path: String,
-    query_string: String,
+    query_string: Option<String>,
     method: Method,
 }
 
