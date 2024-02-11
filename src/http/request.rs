b08c473c380a1142052fc15d2d9f4839acde65b6
m1a2st <s7133700@gmail.com>
2024-02-11 11:30:37 +0800

    feat(error):
    add new Parse error type in request to handle request error

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,50 @@
 use super::method::Method;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult, write};
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    // GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid request",
+            Self::InvalidEncoding => "Invalid encoding",
+            Self::InvalidProtocol => "Invalid protocol",
+            Self::InvalidMethod => "Invalid method",
+        }
+    }
+}
+
+impl Error for ParseError {}
