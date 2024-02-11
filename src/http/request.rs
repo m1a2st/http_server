81ac9c32311090db93714f1c4899bd6cb8666702
m1a2st <s7133700@gmail.com>
2024-02-11 15:10:42 +0800

    feat: add new method error
    add new method error to resolve pares problem

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::str;
 use std::error::Error;
@@ -12,12 +12,20 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     // GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(value)?;
-        todo!()
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
     }
 }
 
@@ -57,6 +65,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid protocol",
             Self::InvalidMethod => "Invalid method",
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
