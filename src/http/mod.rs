38af98025ea824097b6ef0767890cd4137c07afe
m1a2st <s7133700@gmail.com>
2024-02-13 21:52:08 +0800

    feat(query_string): add struct
    add from method in query_string

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,6 +1,7 @@
 pub use request::Request;
 pub use method::Method;
 pub use request::ParseError;
+pub use query_string::{QueryString, Value as QueryStringValue};
 
 mod request;
 mod method;
--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -14,3 +14,31 @@ impl<'buf> QueryString<'buf> {
         self.data.get(key)
     }
 }
+
+// a=1&b=2&c&d=&e===&d=7&d=abc
+impl<'buf> From<&'buf str> for QueryString<'buf> {
+    fn from(value: &str) -> Self {
+        let mut data = HashMap::new();
+        for sub_string in value.split('&') {
+            let mut key = sub_string;
+            let mut val = "";
+            sub_string.find('=').map(|i| {
+                key = &sub_string[..i];
+                val = &sub_string[i + 1..];
+            });
+            data.entry(key)
+                .and_modify(|existing: &mut Value| {
+                    match existing {
+                        Value::Single(prev_val) => {
+                            *existing = Value::Multiple(vec![val, prev_val]);
+                        }
+                        Value::Multiple(vec) => {
+                            vec.push(val);
+                        }
+                    }
+                })
+                .or_insert(Value::Single(val));
+        }
+        QueryString { data }
+    }
+}
